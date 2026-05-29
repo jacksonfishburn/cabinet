b542b37928e2fc97beb982545c7d5862f384f829
jacksonfishburn <jacksonfishburn@gmail.com>
2026-05-29 17:44:09 -0600

    refactor(test): declared containers replace jdbc url

--- backend/src/test/java/com/cabinet/cabinet/CabinetApplicationTests.java
+++ backend/src/test/java/com/cabinet/cabinet/CabinetApplicationTests.java
@@ -2,11 +2,20 @@ package com.cabinet.cabinet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CabinetApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres =
+			new PostgreSQLContainer<>("postgres:17-alpine");
+
 
 	@Test
 	void contextLoads() {
