36a491796871ab55b410033ac875829382ece255
jacksonfishburn <jacksonfishburn@gmail.com>
2026-05-29 17:33:06 -0600

    chore(test): add Postgres containers support

--- backend/src/test/java/com/cabinet/cabinet/CabinetApplicationTests.java
+++ backend/src/test/java/com/cabinet/cabinet/CabinetApplicationTests.java
@@ -2,18 +2,10 @@ package com.cabinet.cabinet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-		"cabinet.storage-dir=target/test-storage",
-		"cabinet.max-size-mb=10",
-		"cabinet.token=test-token",
-		"spring.datasource.url=jdbc:h2:mem:cabinet-test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-		"spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@SpringBootTest
+@ActiveProfiles("test")
 class CabinetApplicationTests {
 
 	@Test
